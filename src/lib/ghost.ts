244b7300ea4f4fa41a81a15749c3a8b9f811100a
charith <tilershub@gmail.com>
2026-01-13 20:30:44 +0530

    Update ghost.ts

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,7 +1,8 @@
 import GhostContentAPI from '@tryghost/content-api';
 
+// This is the "Handshake" between Ghost and Astro
 export const ghostClient = new GhostContentAPI({
   url: import.meta.env.GHOST_API_URL,
-  key: import.meta.meta.env.GHOST_CONTENT_API_KEY,
+  key: import.meta.env.GHOST_CONTENT_API_KEY,
   version: 'v5.0',
 });
