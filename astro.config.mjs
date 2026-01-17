16639709e9c2700a4f140947e8eaf112513a1a53
charith <tilershub@gmail.com>
2026-01-17 22:59:32 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://tradeninety.com', 
   integrations: [svelte(), tailwind()],
   output: 'static'
 });
