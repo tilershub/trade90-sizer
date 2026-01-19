c07ac7004db1ca05f5d77af7b1c2692678bbfc2a
charith <tilershub@gmail.com>
2026-01-20 05:22:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
   site: "https://tradeninety.com",
   integrations: [
+    svelte(),
     sitemap(),
     robotsTxt({
-      policy: [
-        { userAgent: "*", allow: "/" },
-      ],
+      policy: [{ userAgent: "*", allow: "/" }],
     }),
   ],
 });
